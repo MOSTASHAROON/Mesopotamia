6baa3598f051f4e84a9d284811d8c9c41f349ab1
Mohammed Al-Mahdawi <mohammed@mostasharoon.org>
2016-03-05 20:34:51 +0300

    Setup scripts

--- functions.php
+++ functions.php
@@ -8,75 +8,71 @@
  */
 
 if ( ! function_exists( 'mesopotamia_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function mesopotamia_setup() {
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on Mesopotamia, use a find and replace
-	 * to change 'mesopotamia' to the name of your theme in all the template files.
-	 */
-	load_theme_textdomain( 'mesopotamia', get_template_directory() . '/languages' );
-
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
-
-	/*
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
-	add_theme_support( 'title-tag' );
-
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
 	 */
-	add_theme_support( 'post-thumbnails' );
+	function mesopotamia_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on Mesopotamia, use a find and replace
+		 * to change 'mesopotamia' to the name of your theme in all the template files.
+		 */
+		load_theme_textdomain( 'mesopotamia', get_template_directory() . '/languages' );
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => esc_html__( 'Primary', 'mesopotamia' ),
-	) );
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
 
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
-	) );
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
 
-	/*
-	 * Enable support for Post Formats.
-	 * See https://developer.wordpress.org/themes/functionality/post-formats/
-	 */
-	add_theme_support( 'post-formats', array(
-		'aside',
-		'image',
-		'video',
-		'quote',
-		'link',
-	) );
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'mesopotamia_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
-}
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menus( array(
+			'primary' => esc_html__( 'Primary', 'mesopotamia' ),
+		) );
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		) );
+
+		/*
+		 * Enable support for Post Formats.
+		 * See https://developer.wordpress.org/themes/functionality/post-formats/
+		 */
+		add_theme_support( 'post-formats', array(
+			'aside'
+		) );
+
+		// Set up the WordPress core custom background feature.
+//		add_theme_support( 'custom-background', apply_filters( 'mesopotamia_custom_background_args', array(
+//			'default-color' => 'ffffff',
+//			'default-image' => '',
+//		) ) );
+	}
 endif;
 add_action( 'after_setup_theme', 'mesopotamia_setup' );
 
@@ -90,6 +86,7 @@ add_action( 'after_setup_theme', 'mesopotamia_setup' );
 function mesopotamia_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'mesopotamia_content_width', 640 );
 }
+
 add_action( 'after_setup_theme', 'mesopotamia_content_width', 0 );
 
 /**
@@ -108,6 +105,7 @@ function mesopotamia_widgets_init() {
 		'after_title'   => '</h2>',
 	) );
 }
+
 add_action( 'widgets_init', 'mesopotamia_widgets_init' );
 
 /**
@@ -123,7 +121,15 @@ function mesopotamia_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
+	wp_enqueue_style( 'mesopotamia-bootstrap', get_template_directory_uri() . '/lib/bootstrap/css/bootstrap.min.css' );
+	wp_enqueue_style( 'mesopotamia-bootstrap-theme', get_template_directory_uri() . '/lib/bootstrap/css/bootstrap-theme.min.css', array(
+		'mesopotamia-bootstrap'
+	) );
+
+	wp_enqueue_script( 'mesopotamia-bootstrap-js', get_template_directory_uri() . '/lib/bootstrap/js/bootstrap.min.js', array( 'jquery' ) );
 }
+
 add_action( 'wp_enqueue_scripts', 'mesopotamia_scripts' );
 
 /**
