ebf29f3cf66f1b463d01c9e75f9d328384b40791
Mohammed Al-Mahdawi <mohammed@mostasharoon.org>
2017-03-15 16:11:55 +0300

    Add singular placeholder, needed for some languages

--- comments.php
+++ comments.php
@@ -25,53 +25,64 @@ if ( post_password_required() ) {
 	<?php
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) : ?>
-		<h2 class="comments-title">
+        <h2 class="comments-title">
 			<?php
 			printf( // WPCS: XSS OK.
-				esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'mesopotamia' ) ),
+				esc_html(
+					_nx(
+						'%1$s thought on &ldquo;%2$s&rdquo;',
+						'%1$s thoughts on &ldquo;%2$s&rdquo;',
+						get_comments_number(),
+						'comments title',
+						'mesopotamia'
+					) ),
 				number_format_i18n( get_comments_number() ),
 				'<span>' . get_the_title() . '</span>'
 			);
 			?>
-		</h2>
+        </h2>
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-			<nav id="comment-nav-above" class="clearfix navigation comment-navigation" role="navigation">
-				<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'mesopotamia' ); ?></h2>
-				<div class="nav-links">
+            <nav id="comment-nav-above" class="clearfix navigation comment-navigation" role="navigation">
+                <h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'mesopotamia' ); ?></h2>
+                <div class="nav-links">
 
-					<div
-						class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'mesopotamia' ) ); ?></div>
-					<div
-						class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'mesopotamia' ) ); ?></div>
+                    <div
+                            class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments',
+							'mesopotamia' ) ); ?></div>
+                    <div
+                            class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments',
+							'mesopotamia' ) ); ?></div>
 
-				</div><!-- .nav-links -->
-			</nav><!-- #comment-nav-above -->
+                </div><!-- .nav-links -->
+            </nav><!-- #comment-nav-above -->
 		<?php endif; // Check for comment navigation. ?>
 
-		<ol class="comment-list media-list">
+        <ol class="comment-list media-list">
 			<?php
 			wp_list_comments( array(
-				'style'      => 'ol',
-				'short_ping' => true,
-				'avatar_size'=>48,
-				'callback' => 'bootstrap3_comment_format',
+				'style'       => 'ol',
+				'short_ping'  => true,
+				'avatar_size' => 48,
+				'callback'    => 'bootstrap3_comment_format',
 			) );
 			?>
-		</ol><!-- .comment-list -->
+        </ol><!-- .comment-list -->
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-			<nav id="comment-nav-below" class="clearfix navigation comment-navigation" role="navigation">
-				<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'mesopotamia' ); ?></h2>
-				<div class="nav-links">
+            <nav id="comment-nav-below" class="clearfix navigation comment-navigation" role="navigation">
+                <h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'mesopotamia' ); ?></h2>
+                <div class="nav-links">
 
-					<div
-						class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'mesopotamia' ) ); ?></div>
-					<div
-						class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'mesopotamia' ) ); ?></div>
+                    <div
+                            class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments',
+							'mesopotamia' ) ); ?></div>
+                    <div
+                            class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments',
+							'mesopotamia' ) ); ?></div>
 
-				</div><!-- .nav-links -->
-			</nav><!-- #comment-nav-below -->
+                </div><!-- .nav-links -->
+            </nav><!-- #comment-nav-below -->
 			<?php
 		endif; // Check for comment navigation.
 
@@ -81,7 +92,7 @@ if ( post_password_required() ) {
 	// If comments are closed and there are comments, let's leave a little note, shall we?
 	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
 
-		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'mesopotamia' ); ?></p>
+        <p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'mesopotamia' ); ?></p>
 		<?php
 	endif;
 
